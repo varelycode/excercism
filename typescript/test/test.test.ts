e2f1caa5bf24fc2bd9e0369e92c50895068fd236
Viviana Rosas <viviana.rosas@atomicobject.com>
2021-05-13 09:36:16 -0700

    fixed tests

--- typescript/test/test.test.ts
+++ typescript/test/test.test.ts
@@ -2,11 +2,16 @@ import OtherPerson from "./test";
 import Person from "./test";
 
 describe("test()", () => {
-  const words = new OtherPerson("Jamie", "Adams");
-
   it("Can Create a New class with names", () => {
-    const expectedCounts = new Map(Object.entries({ word: 1 }));
-    expect(words.firstName).toEqual("Jamie");
-    expect(words.lastName).toEqual("Adams");
+    const person = new OtherPerson("Jamie", "Adams");
+    expect(person.firstName).toEqual("Jamie");
+    expect(person.lastName).toEqual("Adams");
+  });
+  it("Can do partial confirm to OtherPerson", () => {
+    const otherPerson: Partial<OtherPerson> = {
+      firstName: "Jamie",
+      lastName: "Adams",
+    };
+    expect(otherPerson.sayHi).toEqual(undefined);
   });
 });
